8ccc1854ad6ac2ba1befc51d90b187d8119c1e25
adityassrana <adityassrana2@gmail.com>
2020-10-21 19:07:40 +0530

    add gradient descent code

---
+++ week2/grad_descent.m
@@ -0,0 +1,18 @@
+function [x] = grad_descent(A, b, x)
+    
+r = b - A*x;
+err = norm(r);
+
+tol = 1e-2;
+i = 1;
+while err > tol
+    q = A*r;
+    alpha = (transpose(r)*r) /(transpose(q)*r);
+    r = b - A*x; 
+    x = x + alpha*r;
+    
+    err = norm(r);
+    fprintf('iter %03d, error: %.6f\n', i, err);
+    
+    i = i+1;
+end
